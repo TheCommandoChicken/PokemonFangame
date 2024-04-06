17026cb8dc7b130750cd0db80738f147d3a1af0b
TheCommandoChicken <62488159+TheCommandoChicken@users.noreply.github.com>
2024-04-06 11:50:46 -0300

    Attempting to fix regression

--- script/base_pokemon.gd
+++ script/base_pokemon.gd
@@ -5,8 +5,9 @@ extends Node
 func _ready() -> void:
 	var path = "res://resource/pokemon.json"
 	if not FileAccess.file_exists(path):
+		print("no pokemon data found")
 		return
 	var file = FileAccess.open(path, FileAccess.READ)
 	var test_json_conv = JSON.new()
-	test_json_conv.parse(file.get_as_text())
+	test_json_conv.parse(file.get_as_text(true))
 	pokemon_table = test_json_conv.get_data()
