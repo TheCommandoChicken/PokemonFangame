649d82f21bd3ddab84c28bbd05c88c8d1b8e8d34
TheCommandoChicken <62488159+TheCommandoChicken@users.noreply.github.com>
2024-04-15 17:40:02 -0300

    Improving movement

--- script/common/player.gd
+++ script/common/player.gd
@@ -1,40 +1,61 @@
 extends CharacterBody3D
 
-signal entered_tile(player : CharacterBody3D)
+signal stepped(areas : Array[Area3D], encounter_factor : int, player : CharacterBody3D)
 
 @export var walk_speed : float
 @export var run_speed : float
+@export var max_encounter_factor : int
 const tile_size : float = 24
 var last_dir : Vector3
 var distance : float = 0.0
 var speed : float
 var dir_list : Array = [""]
+var encounter_factor : int
+var delay_frames : int
+var buffer_frames : int
+var facing_dir : Vector3
 
-func _physics_process(delta):
+func _physics_process(_delta):
+	delay_frames -= 1
+	$DebugArrow.rotation.y = atan2(facing_dir.x, facing_dir.z)
+	
+	if encounter_factor <= 0:
+		encounter_factor = max_encounter_factor
+	
 	for dir in ["up", "down", "left", "right"]:
 		if Input.is_action_just_pressed(dir):
 			dir_list.append(dir)
+			delay_frames = 7
 		elif Input.is_action_just_released(dir):
 			dir_list.remove_at(dir_list.find(dir))
 	
 	if distance == 0.0:
 		if (Input.is_action_pressed("secondary_button") and !Settings.autorun) or (!Input.is_action_pressed("secondary_button") and Settings.autorun):
-			speed = run_speed
+			if speed != run_speed:
+				speed = run_speed
+				encounter_factor = max(encounter_factor - max_encounter_factor / 2, 0)
 		else:
-			speed = walk_speed
-	
-		last_dir = get_input_direction(dir_list[dir_list.size() - 1])
+			if speed != walk_speed:
+				speed = walk_speed
+				encounter_factor = min(encounter_factor + max_encounter_factor / 2, max_encounter_factor)
 		
-		if last_dir != Vector3.ZERO:
-			distance = tile_size
+		if delay_frames <= 0:
+			last_dir = get_input_direction(dir_list[dir_list.size() - 1])
+			
+			if last_dir != Vector3.ZERO:
+				distance = tile_size
+				facing_dir = last_dir
 	
 	if distance > 0.0:
 		velocity = last_dir * speed
 		move_and_slide()
 		distance -= speed
-	
-	if distance < 0.0:
-		distance = 0.0
+		if distance <= 0.0:
+			emit_signal("stepped", $Area.get_overlapping_areas(), encounter_factor, self)
+			print("stepped")
+			print(encounter_factor)
+			encounter_factor -= 1
+			distance = 0.0
 	
 func get_input_direction(dir : String) -> Vector3:
 	match dir:
--- script/encounter_area.gd
+++ script/encounter_area.gd
@@ -4,14 +4,21 @@ signal triggered_encounter(pokemon : BasePokemon, level : int, player : Characte
 
 @export var encounters : EncounterTable
 @export_range(0, 255) var encounter_rate : int
+@export var player : CharacterBody3D
 
-func _on_player_entered_tile(player : CharacterBody3D):
-	if overlaps_body(player) and randi_range(1, 255) <= encounter_rate:
+func _ready():
+	player.connect("stepped", Callable(self, "_on_player_stepped"))
+
+func _on_player_stepped(areas : Array[Area3D], encounter_factor : int, player : CharacterBody3D):
+	if self in areas and randi_range(1, 255) + encounter_factor <= encounter_rate:
+		print("these nutes")
 		var id = get_encounter()
-		emit_signal("triggered_encounter", encounters.pokemon[id], encounters.levels[id], player)
+		player.encounter_factor = 0
+		emit_signal("triggered_encounter", encounters.pokemon[id], encounters.levels[id])
+		print("You would have an encounter with ", encounters.pokemon[id], " here, but I haven't added that yet!")
 
 func get_encounter() -> int:
-	var sum_weights : int
+	var sum_weights : int = 0
 	
 	for i in encounters.weights:
 		sum_weights += i
